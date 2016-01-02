d49cc3571552b72142d008a4b94c2e9801e76078
ayub.malik <ayub.malik@gmail.com>
2016-01-02 21:02:00 +0000

    wip

--- ncurses/sample5_color.c
+++ ncurses/sample5_color.c
@@ -12,11 +12,14 @@ WINDOW *create_win(int height, int width, int starty, int startx);
 void destroy_win(WINDOW *local_win);
 
 int main() {
-  //setlocale(LC_ALL,"");
+  setlocale(LC_ALL,"");
   WINDOW *win;
   check(win = initscr(), "could not initscr())");
   curs_set(0);
-  char msg[] = "Hi ";
+  start_color();
+  init_pair(1, COLOR_WHITE, COLOR_BLACK);
+  init_pair(2, COLOR_RED, COLOR_BLACK);
+  char msg[] = "Hola!";
   int y=4, x=4;
   for(int i=0; i < 32; i += 4) {
     WINDOW *nwin = create_win(3, strlen(msg) + 5, y+i % 32, x+i % 32);
@@ -34,17 +37,19 @@ error:
 }
 
 WINDOW *create_win(int height, int width, int starty, int startx) {
-  WINDOW *local_win;
-  local_win = newwin(height, width, starty, startx);
-  box(local_win, 0 , 0);    
-  wrefresh(local_win); 
-  return local_win;
+  WINDOW *win;
+  win = newwin(height, width, starty, startx);
+  //wborder(local_win, LS, RS, TS, BS, TL, TR, BL, BR);
+  wattron(win, COLOR_PAIR(2));
+  box(win, 0, 0);
+  wattroff(win,COLOR_PAIR);
+  wrefresh(win); 
+  return win;
 }  
 
-void destroy_win(WINDOW *local_win) { 
-//  wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-  wclear(local_win);
-  wrefresh(local_win);
-  delwin(local_win);
+void destroy_win(WINDOW *win) { 
+  wclear(win);
+  wrefresh(win);
+  delwin(win);
 }
 
